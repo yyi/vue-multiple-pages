2c2f7badd68865209c4ec700de2ed3884f916373
yyi.mailer@gmail.com <yangyigood108>
2018-12-21 15:20:36 +0800

    update multiple vue instance

--- src/pages/user/login/app.js
+++ src/pages/user/login/app.js
@@ -5,7 +5,11 @@ import App from './app.vue'
 
 Vue.use(ElementUI)
 
-new Vue({
+let vm1 = new Vue({
   el: '#app',
   render: h => h(App)
 })
+setTimeout(() => {
+  let vm2 = new Vue(App).$mount('#app2').login()
+  console.log(vm1 !== vm2)
+}, 5000)
--- src/pages/user/login/app.vue
+++ src/pages/user/login/app.vue
@@ -1,21 +1,25 @@
 <template>
-  <el-card class="login-card">
-    <div slot="header">
-      <img :src="logo" class="logo-img">
-      <span class="logo-text">Management System</span>
-    </div>
-    <el-form class="login" label-width="100px" >
-      <el-form-item label="Phone">
-        <el-input placeholder="Please enter phone number" v-model="ruleForm.phone" type="tel" :maxlength="11"></el-input>
-      </el-form-item>
-      <el-form-item label="Password">
-        <el-input placeholder="Please enter password" v-model="ruleForm.password" type="password" @keyup.enter="login"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="login">Login</el-button>
-      </el-form-item>
-    </el-form>
-  </el-card>
+
+    <el-card class="login-card">
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+        <div slot="header">
+            <img :src="logo" class="logo-img">
+            <span class="logo-text">Management System</span>
+        </div>
+        <el-form class="login" label-width="100px">
+            <el-form-item label="Phone">
+                <el-input placeholder="Please enter phone number" v-model="ruleForm.phone" type="tel"
+                          :maxlength="11"></el-input>
+            </el-form-item>
+            <el-form-item label="Password">
+                <el-input placeholder="Please enter password" v-model="ruleForm.password" type="password"
+                          @keyup.enter="login"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="login">Login</el-button>
+            </el-form-item>
+        </el-form>
+    </el-card>
 </template>
 
 <script>
@@ -28,7 +32,8 @@ export default {
         phone: '',
         password: ''
       },
-      logo: image
+      logo: image,
+      input: ''
     }
   },
 
@@ -45,14 +50,17 @@ export default {
   width: 800px;
   margin: 100px auto;
 }
+
 .login {
   display: block;
   width: 400px;
   margin: 100px auto;
 }
+
 .logo-img {
   width: 90px;
 }
+
 .logo-text {
   margin-left: 20px;
 }
